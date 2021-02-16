1b020510aa77ba6cab462d8800977dd3c9e09722
lessacaires <lessacaires@gmail.com>
2021-02-16 15:09:45 -0300

    Add files via upload

---
+++ Curso Python Avançado/Jogo Mario Bros/data/constants.py
@@ -0,0 +1,58 @@
+SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 800
+
+SCREEN_SIZE = (SCREEN_WIDTH, SCREEN_HEIGHT)
+
+TITLE = "Super Mario Bros"
+
+#CORES
+
+WHITE           = (255,255,255)
+RED             = (255,0,0)
+GRAY            = (100,100,100)
+NAVBLUE         = (60,60,100)
+FOREST_GREEM    = (31,162,35)
+BLUE            = (0,255,0)
+SKY_BLUE        = (39,145,251)
+YELLOW          = (255,255,0)
+ORANGE          = (255,128,0)
+PURPLE          = (255,0,255)
+CYAN            = (0,255,255)
+BLACK           = (0,0,0)
+NEAR_BLACK      = (19,15,48)
+COMBLUE         = (233,233,255)
+GOLD            = (255,215,0)
+
+BGCOLOR = WHITE
+
+SIZE_MULTIPLIER = 2.5
+BRICK_SIZE_MULTIPLIER = 2.69
+BACKGROUND_MULTIPLIER = 2.679
+GROUND_HEIGHT = SCREEN_HEIGHT - 62
+
+#MARIO FORÇAS
+WLAK_ACCEL = 0,15
+RUN_ACCEL = 20
+SMALL_TURNAROUND = 0.35
+
+GRAVITY = 1.01
+JUMP_GRAVITY = 0.31
+JUMP_VEL = -10
+FAST_JUMP_VEL = -12.5
+MAX_Y_VEL = 11
+
+MAR_RUN_SPEED = 800
+MAX_WALK_SPEED = 6
+
+#MARIO ESTADOS
+
+STAND = 'standing'
+WALK =  'walk'
+JUMP = 'jump'
+FALL = 'fall'
+SMALL_TO_BIG = 'smal to big'
+BIG_TO_FIRE = 'big to fire'
+BIG_TO_SMALL = 'big to small'
+FLAGPOLE = 'flag pole'
+WALKING_TO_CASTLE = 'walking to castle'
+END_OF_LEVEL_FALL = 'end of level fall'
